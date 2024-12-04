10bada7c80a47b56917af8e2202e988a89f65d44
Steve <huuhieu1711@gmail.com>
2024-12-04 12:36:45 +0700

    inital Vue3 App

---
+++ src/layouts/GuestLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>App Layout Guest</h1>
+    <slot />
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "GuestLayout",
+}
+</script>
+
+<style scoped></style>
---
+++ src/layouts/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <component :is="this.$route.meta.layoutComponent">
+    <slot></slot>
+  </component>
+
+</template>
+<script lang="ts">
+import { RouterView, RouterLink } from 'vue-router'
+export default {
+  name: "Layout",
+}
+
+</script>
