b45cab268ab553d7289ee2003829852678b44b49
Steve <huuhieu1711@gmail.com>
2024-12-05 19:14:27 +0700

    chore: proxy api

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  build: {
-    target: 'esnext', // Không cần kiểm tra TypeScript
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  const API_URL = `${env.VITE_API_URL ?? 'http://localhost:3000'}`
+  return {
+    server: {
+      proxy: {
+        // 选项写法
+        '/v1/api': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^v1\/api/, ''),
+        },
+      },
     },
-  },
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    build: {
+      target: 'esnext', // Không cần kiểm tra TypeScript
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
