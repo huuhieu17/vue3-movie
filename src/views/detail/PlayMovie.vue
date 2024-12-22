1bd55dbaeace9cfac49e7843747356e98f0ebde5
Steve <huuhieu1711@gmail.com>
2024-12-22 17:50:24 +0700

    fix: scroll into view when change episode

--- src/views/detail/PlayMovie.vue
+++ src/views/detail/PlayMovie.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router'
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, onMounted, reactive, ref, watch } from 'vue'
 import httpClient from '@/utils/httpClient.ts'
 import type { ResponseMovie } from '@/interfaces/home.ts'
 import { VideoPlayer } from '@videojs-player/vue'
 import { NCollapse, NCollapseItem } from 'naive-ui'
 import NewFilm from '@/views/home/NewFilm.vue'
 import { appConfig } from '@/utils/config.ts'
+
+import 'videojs-landscape-fullscreen'
 
 const route = useRoute()
 const router = useRouter()
@@ -15,15 +17,26 @@ const slug = route.params.slug
 const episode = computed(() => route.query.episode)
 const movieData = ref<ResponseMovie | null>(null)
 const listEpisodeData = ref<any>([])
-const videoPlayer = ref(null)
+const videoPlayer = ref<any>(null)
 const currentEpisode = ref<any>(null)
 
-const playerOptions = ref<any>({
-  autoplay: false,
-  controls: true,
-  controlBar: { timeDivider: false, durationDisplay: false },
-  sources: [],
-})
+const episodeRefs = reactive<any>([])
+
+const playerOptions = ref<any>({})
+
+const updateRefs = (el: any, index: number) => {
+  episodeRefs[index] = el
+}
+
+const updateMetaTitle = () => {
+  if (movieData.value && currentEpisode.value) {
+    document.title = `${movieData.value.item.name} - Tập ${currentEpisode.value.name}`
+  } else if (movieData.value) {
+    document.title = movieData.value.item.name
+  } else {
+    document.title = 'Xem Phim Online'
+  }
+}
 
 const playEspisode = (_episode: string) => {
   router.push({
@@ -39,8 +52,23 @@ const playEspisode = (_episode: string) => {
     playerOptions.value = {
       autoplay: true,
       controls: true,
-      controlBar: { timeDivider: false, durationDisplay: false },
-      poster: movieData.value && appConfig.imageUrl + '/uploads/movies/' + movieData.value.item.poster_url,
+      playbackRates: [0.5, 1, 1.5, 2],
+      controlBar: {
+        timeDivider: true,
+        durationDisplay: false,
+        remainingTimeDisplay: false,
+        currentTimeDisplay: true,
+        volumeControl: true,
+        playToggle: true,
+        progressControl: true,
+        fullscreenToggle: true,
+        skipButtons: true,
+      },
+      enableSmoothSeeking: true,
+      plugins: {},
+      poster:
+        movieData.value &&
+        appConfig.imageUrl + '/uploads/movies/' + movieData.value.item.poster_url,
       sources: [
         {
           src: episodeByQuery.servers[0].link_m3u8,
@@ -48,6 +76,26 @@ const playEspisode = (_episode: string) => {
         },
       ],
     }
+  }
+  updateMetaTitle()
+}
+
+const scrollToActiveEpisode = () => {
+  const activeEpisodeIndex = listEpisodeData.value.findIndex(
+    (item: any) => item.slug === currentEpisode.value?.slug,
+  )
+  if (activeEpisodeIndex !== -1 && episodeRefs && episodeRefs[activeEpisodeIndex]) {
+    const parentElement = episodeRefs[activeEpisodeIndex].parentElement
+    const parentRect = parentElement.getBoundingClientRect()
+    const childRect = episodeRefs[activeEpisodeIndex].getBoundingClientRect()
+
+    // Tính khoảng cách giữa thẻ con và thẻ cha
+    const topDistance = childRect.top - parentRect.top
+    parentElement.scrollTo({
+      top: topDistance,
+      behavior: 'smooth',
+      animated: true,
+    })
   }
 }
 
@@ -79,40 +127,63 @@ onMounted(async () => {
   }
 })
 
-watch(playerOptions, () => {
-  // When playerOptions changes, update the VideoPlayer component
+watch(episodeRefs, scrollToActiveEpisode)
+
+watch(currentEpisode, () => {
   if (videoPlayer.value) {
+    videoPlayer.value.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      inline: 'center',
+    })
   }
 })
 </script>
 
 <template>
-  <div class="mt-16 lg:px-20 px-2 lg:flex block" v-if="movieData">
-    <div class="lg:w-4/5 w-full">
-      <div class="video-player w-full">
-        <VideoPlayer class="!w-full !h-auto aspect-video" ref="videoPlayer" :key="playerOptions"
-          :options="playerOptions" />
+  <div class="mt-16 xl:px-20 px-2 xl:flex block" v-if="movieData && playerOptions">
+    <div class="xl:w-4/5 w-full">
+      <div
+        class="video-player w-full"
+        :ref="
+          (el) => {
+            videoPlayer = el
+          }
+        "
+      >
+        <VideoPlayer
+          class="!w-full !h-auto aspect-video"
+          id="video-player"
+          :key="playerOptions"
+          :options="playerOptions"
+        />
       </div>
     </div>
-    <div class="lg:w-1/5 w-full lg:px-4 lg:mt-0 mt-5">
-      <div class="text-2xl">
-        {{ movieData.item.name }} - Tập {{ currentEpisode.name }}
-      </div>
+    <div class="xl:w-1/5 w-full lg:px-4 xl:mt-0 mt-5">
+      <div class="text-2xl">{{ movieData.item.name }} - Tập {{ currentEpisode.name }}</div>
       <n-collapse class="mt-4" default-expanded-names="1">
         <div v-if="currentEpisode && currentEpisode.servers">
           <div class="mb-2">Server</div>
           <div class="w-full flex flex-wrap gap-4">
-            <div class="bg-gray-900 px-5 py-2 cursor-pointer" v-for="(item, index) in currentEpisode.servers"
-              :key="index">
+            <div
+              class="bg-gray-900 px-5 py-2 cursor-pointer"
+              v-for="(item, index) in currentEpisode.servers"
+              :key="index"
+            >
               {{ item.name }}
             </div>
           </div>
         </div>
         <n-collapse-item class="font-bold" title="Danh sách tập" name="1">
-          <div class="w-full flex flex-wrap gap-4">
-            <div @click="playEspisode(item.slug)" class="px-5 py-2 cursor-pointer text-sm"
-              :class="currentEpisode.slug == item.slug ? 'bg-gray-500' : 'bg-gray-900'" v-for="item in listEpisodeData"
-              :key="item.name">
+          <div class="w-full flex flex-wrap gap-4 max-h-[65vh] overflow-y-auto">
+            <div
+              @click="playEspisode(item.slug)"
+              :ref="(el) => updateRefs(el, index)"
+              class="px-5 py-2 cursor-pointer text-sm"
+              :class="currentEpisode.slug == item.slug ? 'bg-gray-500' : 'bg-gray-900'"
+              v-for="(item, index) in listEpisodeData"
+              :key="item.name"
+            >
               {{ item.name }}
             </div>
           </div>
@@ -120,17 +191,19 @@ watch(playerOptions, () => {
       </n-collapse>
     </div>
   </div>
-  <div class="mt-5 lg:px-20 px-2" v-if="movieData">
+  <div class="mt-5 xl:px-20 px-2" v-if="movieData">
     <div class="lg:text-2xl">{{ movieData.item.name }} - {{ movieData.item.origin_name }}</div>
     <div class="flex items-center mt-2 flex-wrap">
-      {{ movieData.item.year }} | {{ movieData.item.time }} | <RouterLink class="mx-2"
-        :to="'/quoc-gia?country=' + movieData.item.country[0].slug">{{ ' ' + movieData.item.country[0].name + ' ' }}
-      </RouterLink> |
+      {{ movieData.item.year }} | {{ movieData.item.time }} |
+      <RouterLink class="mx-2" :to="'/quoc-gia?country=' + movieData.item.country[0].slug">
+        {{ ' ' + movieData.item.country[0].name + ' ' }}
+      </RouterLink>
+      |
       {{ movieData.item.episode_current }} |
       <span class="ml-1" v-for="cate in movieData.item.category" :key="cate.id">
-        <RouterLink class="mx-2" :to="'/the-loai?category=' + cate.slug">{{ cate.name }} ·
+        <RouterLink class="mx-2" :to="'/the-loai?category=' + cate.slug"
+          >{{ cate.name }} ·
         </RouterLink>
-
       </span>
     </div>
     <div class="mt-4" v-html="movieData.item.content"></div>
