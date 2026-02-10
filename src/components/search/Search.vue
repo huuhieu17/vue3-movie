eeff151045e1c04aa181e4f711e9ca0438d848f4
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2026-02-10 21:20:10 +0700

    fix: change a tag to routerlink vue

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -8,10 +8,10 @@
     />
 
       <div v-for="item of listSearchData" :key="item._id" class="mb-5 border border-[#414040] rounded-lg cursor-pointer overflow-hidden">
-        <a :href="'/chi-tiet/'+ item.slug" :key="item._id" >
+        <RouterLink :to="'/chi-tiet/'+ item.slug" :key="item._id" >
         <img class="hover:scale-105" :src="`${appConfig.imageUrl}/uploads/movies/${item.poster_url}`" :alt="item.name"/>
         <div class="font-bold my-2 p-2">{{item.name}} - ({{item.origin_name}})</div>
-        </a>
+        </RouterLink>
       </div>
 
 
