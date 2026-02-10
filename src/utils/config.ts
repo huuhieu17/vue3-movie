93ba3d206ccaed4f026b7551e52ea04ed0a1f7b1
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2026-02-10 20:29:36 +0700

    chore: add remove url from env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
 export const appConfig = {
   apiUrl: import.meta.env.VITE_API_URL || '',
+  remoteUrl: import.meta.env.VITE_REMOTE_URL || '',
   imageUrl: ''
 }
--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -3,7 +3,7 @@ import { appConfig } from '@/utils/config.ts'
 
 // Tạo một instance của axios
 const httpClient = axios.create({
-  baseURL: appConfig.apiUrl, // Thay bằng URL API của bạn
+  baseURL: appConfig.remoteUrl, // Thay bằng URL API của bạn
   timeout: 10000, // Thời gian chờ tối đa (10 giây)
   headers: {
     'Content-Type': 'application/json',
